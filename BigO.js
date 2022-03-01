1a93b87bda1d0c2ecb76a728c60f16ef3f0607fa
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-02-28 17:00:46 -0700

    added notes for Divide and Conquer

--- BigO.js
+++ BigO.js
@@ -124,4 +124,52 @@ function validAnagram(first, second) {
 countUniqueValues([1,2,2,5,7,7,99])
 
 //Sliding Window
-//
+// this patttern involves creating a window which can either be an array or number from  one position to anohter 
+//depending on a certain condition, the window either increases or closes and a new window is created 
+//very useful for keeping track of a subset of data in a narray/string ect.
+
+//write a function called maxSubarraySum wish accepts and array of integers and a number called n. 
+//The function should calculate the maximum sum of n consecutive elements in the array
+
+function maxSubarraySum(arr, num) {
+    if (num > arr.length) {
+        return null
+    }
+    var max = -Infinity
+    for(let i = 0; i < arr.length - num + 1; i++){
+        temp = 0
+        for(let j = 0; j < num; j++){
+            temp += arr[i + j]
+        }
+        if (temp > max) {
+            max = temp
+        }
+    }
+    return max
+}
+
+//Better way O(n)
+
+function maxSubarraySum(arr, num){
+    let maxSum = 0
+    let tempSum = 0
+    if (arr.length < num) return null
+    for (let i = 0; i < num; i++){ // create first sum loop num times and add each to maxSum
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum
+}
+maxSubarraySum([2,3,4,5,3,6,5,7], 4)
+
+//Divide and Conquer
+//starts by dividing data up into smaller chuncks
+//given a sorted array of integers, write a function called search, that accepts a value and returns the index where that value is found, if the value isnt found return -1
+
+//Linear Search O(n)
+//Binary Search Log(n) divides up the elements in the middle, gathers the middle and decides which direction to go to.
+
