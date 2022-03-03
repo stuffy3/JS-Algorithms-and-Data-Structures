b8a86686c804be72955a092eb2b61a659b64a8cf
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-03-03 15:14:10 -0700

    added SortingAlgo.js

--- SearchingAlgos.js
+++ SearchingAlgos.js
@@ -19,16 +19,36 @@ function linearSearch(arr, val){
 //if you never find the value return -1
 
 function binarySearch(arr, num) {
- let start = 0
+ let start = 0 // sets the starting point middle point and end point
  let end = arr.length - 1
  let middle = Math.floor((start + end) / 2)
 
- while(arr[middle]  !== num && start <= end) {
+ while(arr[middle]  !== num && start <= end) { //loops through the array  changing the pointers
      if(num < arr[middle]) end = middle-1
      else start = middle + 1
-     middle = Math.floor((start + end) / 2)
+     middle = Math.floor((start + end) / 2) //reassigns the middle number  
  }
- return arr[middle] === num ? middle : -1
+ return arr[middle] === num ? middle : -1 //if the middle number is equal to the number we are searching for then we return the index else -1
 }
 
 binarySearch([2,5,6,8,9,10,11,14,15,16,17,19,36,57], 16)
+
+//Naive String Search
+// 1 loop over the longer string
+// 2 loop over the shorter string
+// 3 if characters don't match, break out of the inner loop
+// 4 if characters do match, keep going
+// 5 if you complee the inner loop and find a match, increment the count of matches
+// 6 return the count
+
+function naiveSearch(long, short) {
+    count = 0
+    for(let i = 0; i < long.length; i++) {
+        for(let j = 0; j < short.length; j++){
+            if(short[j] !== long[i+j]) break;
+            if(j === short.length - 1) count++
+        }
+    }
+
+}
+naiveSearch('lorie loled', 'lol')
