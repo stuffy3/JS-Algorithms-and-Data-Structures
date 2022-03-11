b617dc7606d4d38cfe1bc9e94701e6c2f1fabdac
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-03-11 03:33:16 -0700

    added all functions for singly linked list

--- Data-Sctructures.js
+++ Data-Sctructures.js
@@ -48,6 +48,47 @@
 //increment the length of the list by 1 
 //return the linked list
 
+//GET 
+//this function should accept an "index"
+//If the index is less than zero or greater than or equal to the length of the list, return null
+//loop through the list until you reach the index and return the node at that specific "index"
+
+//SET (changing the value of a node based on its position in the linked list)
+//function should accept a value and an index
+//usingyou get function to find the specific node
+//if the node is not found return false
+//if the node is found set the value of that node to be the value passed to the function and return true
+
+//Insert (adding a node to a linked list at a specific position)
+//if the index is less than zero or grater than the length return false
+//if the index is the same as the length push a new node to the end of the list
+//if the index is 0, unshift a new node to the sart of the list
+//otherwise using the get method, acces the node at the index -1
+//set the next property on that node to be the new node
+//set the next property on the new node to be the previous next
+//incrememnt the length
+//return true
+
+//Remove (removes a node from the linked list at a specific position)
+//if the index is less that zero or greater than the length retrn undefined
+//if the index is the same as the length-1 pop
+//if the index is 0, shift
+//otherwise using the get method, access the node at the index -1 
+//set that next propertyon that node to be the index of the next node
+//decrement the length
+//return the value of the node removed
+
+//REVERSE (Reversing the linked list in place)
+//swap the head and the tail
+//create a variable called next
+//create a variable called prev
+//create  a variable called node and initialize it to the head property
+//loop through the list
+//set next to be the next property on whatever node is
+//set the next property on the node to be whatever prev is 
+//set prev to be the value of the node variable
+//set the node variable to be the value of the next variable
+
 
 class Node{
     constructor(val){
@@ -123,5 +164,58 @@ class SinglyLinkedList{
         this.length++
         return this
 
+    }
+    get(index){
+        if(index < 0 || index >= this.length) return null
+        let counter = 0
+        let current = this.head
+        while(counter !== index){
+            current = current.nextcounter++
+        }
+        return current
+    }
+    set(index, val){
+        let foundNode= this.get(index)
+        if(foundNode){
+            foundNode.val = val
+            return true
+        }
+        return false
+    }
+    insert(index, val){
+        if(index < 0 || index > this.length) return false
+        if(index === this.length) return !!this.push(val)
+        if(index === 0) return !!this.unshift(val)
+        var newNode = new Node(val)
+        var prev = this.get(index - 1)
+        var temp = prev.next
+        prev.next = newNode
+        newNode.next = temp
+        this.length++
+        return true
+    }
+    remove(index){
+        if(index < 0 || index > this.length) return undefined
+        if(index === this.length-1) return this.push(val)
+        if(index === 0) return this.shift(val)
+        var previousNode = this.get(index -1)
+        var removed = previousNode.next
+        previousNode.next = removed.next
+        this.length--
+        return removed
+    }
+    reverse(){
+        var node = this.head
+        this.head = this.tail
+        this.tail = node
+        var next 
+        var prev = null
+        for(let i = 0; i < this.length; i++){
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        }
+        return this
     }
 }
